6f3c17a6ef5bc7444414eb6d427f3056ff5cf698
Jerald Bianan <jeraldbianan@gmail.com>
2024-12-19 01:42:27 +0800

    register routes for ProductionPlan

--- RouteController.php
+++ RouteController.php
@@ -1,29 +1,29 @@
 <?php
 
 /**
- * 
+ *
  * replace the SystemName based on the Folder
- * 
-*/
-namespace App\Http\Controllers\SystemName;
+ *
+ */
+
+namespace App\Http\Controllers\CigProduction;
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Controller; 
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\CigProduction\ProductionPlanController;
 
-class RouteController extends Controller
-{
-    public static function registerRoutes()
-    {
+class RouteController extends Controller {
+    public static function registerRoutes() {
 
-        //rename system-name the system name and ApiController to Module API Controller 
-        Route::prefix('system-name')->middleware(['jwt', 'user-permission'])->group(function () {
-            Route::get('/api/{id?}', [ApiController::class, 'get']); //rename  'api' based on module name ex. /module-name
-            Route::post('/api', [ApiController::class, 'post']); //rename  'api' based on module name ex. /module-name
-            Route::put('/api/{id}', [ApiController::class, 'put']); //rename  'api' based on module name ex. /module-name/{id}
-            Route::delete('/api/{id}', [ApiController::class, 'delete']); //rename  'api' based on module name ex. /module-name{id}
-            Route::post('/api/{id}', [ApiController::class,'upload']); //rename  'api' based on module name ex. /module-name{id}
-            // Add other routes for other ApiController as needed
+        //rename system-name the system name and ApiController to Module API Controller
+        Route::prefix('cig-production')->group(function () {
+            Route::get('/production-plan/production-planning/{id?}', [ProductionPlanController::class, 'getPlan']);
+            Route::post('/production-plan/production-planning', [ProductionPlanController::class, 'postPlan']);
+            Route::put('/production-plan/production-planning/{id}', [ProductionPlanController::class, 'editPlanDetails']);
+            Route::put('/production-plan/production-planning/edit-status/{id}', [ProductionPlanController::class, 'editPlanStatus']);
+            Route::put('/production-plan/production-planning/archive/{id}', [ProductionPlanController::class, 'archiveProductionPlan']); //archive is an extra verb, and the verb is not included in the REQUEST VERBS (GET, POST, PUT, DELETE)
+
+            //NEW ROUTE NAMING CONVENTION - rename  'api' to /module-name/tablename/extra verb if the verb is not included in the REQUEST VERBS (GET, POST, PUT, DELETE)
         });
-
     }
 }
